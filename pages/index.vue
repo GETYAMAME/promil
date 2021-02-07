c054f82e565965ca5ec118a9e31acb497b8a5b5b
GETYAMAME <pon.mikan10@gmail.com>
2021-02-07 23:22:46 +0900

    saved

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,79 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        promil
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <center>
+        <h1>Nuxt.js + uppy example</h1>
+        <v-btn id="select-files" color="primary">upload</v-btn>
+      </center>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
+import Uppy from '@uppy/core'
+import XHRUpload from '@uppy/xhr-upload'
+import Dashboard from '@uppy/dashboard'
+import Webcam from '@uppy/webcam'
 export default {
-  components: {
-    AppLogo
-  }
+  data() {
+    return {
+      uppyId: 'uppy-trigger'
+    }
+  },
+  mounted() {
+    const uppy = Uppy()
+      .use(Dashboard, {
+        trigger: '#select-files'
+      })
+      .use(Webcam, { target: Dashboard }) // ウェブカメラを追加
+      .use(XHRUpload, { endpoint: 'https://api2.transloadit.com' }) // ダミーのURLへアップロード
+    uppy.on('complete', result => {
+      // eslint-disable-next-line no-console
+      console.log(
+        'Upload complete! We’ve uploaded these files:',
+        result.successful
+      )
+    })
+  },
+  methods: {
+    upload(e){
+      const file = e.target.files[0]
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        this.generateImgUrl(e.target.result)
+      }
+      reader.readAsDataURL(file)
+    },
+    load () {
+      var image = new Image();
+      image.src = './canva.jpg';
+      image.onload = (function () {
+        //画像ロードが完了してからキャンバスの準備をする
+        var canvas = document.getElementById("preview");
+        var ctx = canvas.getContext('2d');
+        //キャンバスのサイズを画像サイズに合わせる
+        canvas.width = image.width;
+        canvas.height = image.height;
+        //キャンバスに画像を描画（開始位置0,0）
+        ctx.drawImage(image, 0, 0);
+      });
+    },
+    drawText (canvas_id,text_id) {
+      var canvas = document.getElementById(canvas_id);
+      var ctx = canvas.getContext('2d');
+      var text = document.getElementById(text_id);
+      //文字のスタイルを指定
+      ctx.font = '32px serif';
+      ctx.fillStyle = '#404040';
+      //文字の配置を指定（左上基準にしたければtop/leftだが、文字の中心座標を指定するのでcenter
+      ctx.textBaseline = 'center';
+      ctx.textAlign = 'center';
+      //座標を指定して文字を描く（座標は画像の中心に）
+      var x = (canvas.width / 2);
+      var y = (canvas.height / 2);
+      ctx.fillText(text.value, x, y);
+    },
+  },
 }
 </script>
 
